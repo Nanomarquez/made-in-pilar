a8038f67e425f8b51b1c1472c74753f0c80cead4
Nanomarquez <nanomarqueztres@gmail.com>
2024-11-26 17:44:34 -0300

    capacitor config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.madeinpilar.app',
   appName: 'madeinpilar',
-  webDir: 'out'
+  webDir: 'out',
+  server: {
+    cleartext: true,
+    allowNavigation: ["made-in-pilar-swfo.vercel.app"], // Permitir navegación a tu dominio
+  },
 };
 
 export default config;
