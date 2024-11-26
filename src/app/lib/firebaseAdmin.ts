ce7592e64c7c4523d83b326770b87508e6392486
Nanomarquez <nanomarqueztres@gmail.com>
2024-11-26 16:01:13 -0300

    to deploy

--- src/app/lib/firebaseAdmin.ts
+++ src/app/lib/firebaseAdmin.ts
@@ -5,7 +5,7 @@ const app = !getApps().length
   ? initializeApp({
       credential: cert({
         clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY ? process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY.replace(/\\n/gm, "\n") : undefined,
+        privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
         projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
       }),
     })
