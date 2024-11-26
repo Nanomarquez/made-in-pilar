71cf13ab464042c67df13c6051c581b7d063ca83
Nanomarquez <nanomarqueztres@gmail.com>
2024-11-26 16:12:23 -0300

    meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,10 +21,37 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <title>MIP</title>
+        <link rel="icon" href="/icon.ico" />
+        <meta property="og:image" content="/icon.ico"></meta>
+        <meta
+          name="description"
+          content="Plataforma para gestionar reservas de sala de estudio Made in Pilar"
+        ></meta>
+        <meta name="author" content="Federico Marquez"></meta>
+        <meta
+          name="copyright"
+          content="MIP. Todos los derechos reservados."
+        ></meta>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        ></meta>
+        <meta name="robots" content="index, follow"></meta>
+        <meta property="og:title" content="MIP"></meta>
+        <meta
+          property="og:description"
+          content="Plataforma para gestionar reservas de sala de estudio Made in Pilar"
+        ></meta>
+        <meta name="twitter:title" content="MIP"></meta>
+        <meta
+          name="twitter:description"
+          content="Plataforma para gestionar reservas de sala de estudio Made in Pilar"
+        ></meta>
+        <meta name="twitter:card" content="summary"></meta>
       </head>
       <body className={`antialiased h-screen w-screen bg-[#0a0a0a]`}>
         <Providers>
-        <DeviceDetector />
+          <DeviceDetector />
           <Spinner></Spinner>
           <AuthenticatedLayout>
             <Image
